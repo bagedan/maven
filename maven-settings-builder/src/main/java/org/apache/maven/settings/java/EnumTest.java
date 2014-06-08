34e4ea870f915d21134967b2c405e0fa4536960d
Bogdan Tkach <bagedan@gmail.com>
2014-06-08 18:53:15 +0800

    test commit

---
+++ maven-settings-builder/src/main/java/org/apache/maven/settings/java/EnumTest.java
@@ -0,0 +1,17 @@
+package org.apache.maven.settings.java;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tkachi
+ * Date: 08.06.14
+ * Time: 18:44
+ * To change this template use File | Settings | File Templates.
+ */
+public enum EnumTest {
+    A(1), B(2);
+
+    private int a;
+    EnumTest(int i) {
+         a= i;
+    }
+}
---
+++ maven-settings-builder/src/main/java/org/apache/maven/settings/java/Example.java
@@ -0,0 +1,25 @@
+package org.apache.maven.settings.java;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tkachi
+ * Date: 08.06.14
+ * Time: 18:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class Example {
+    private int a;
+
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void test(){
+        EnumTest enum = new EnumTest(3);
+    }
+}
